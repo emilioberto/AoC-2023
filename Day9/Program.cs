a1f5e4f8c498a9157fceebb2165507e044de34b8
Emilio Bertò <emilio.berto1@gmail.com>
2023-12-14 23:46:17 +0100

    Spaghetti code but it works :D

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -2,46 +2,60 @@
 
 var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 
-var historyRegex = new Regex(@"\d+");
-
+var historyRegex = new Regex(@"\-?\d+");
+var part1Result = 0m;
+var part2Result = 0m;
+var row = 0;
 await foreach (var line in File.ReadLinesAsync(filePath))
 {
+    row++;
     var matches = historyRegex.Matches(line);
-    var numbers = matches.Select(e => e.Value).Select(long.Parse).ToArray();
+    var numbers = matches.Select(e => e.Value).Select(long.Parse).ToList();
 
-    var differences = GetDifferencesArray(numbers);
+    var part1Lines = new List<long>();
+    var part2Lines = new List<long>();
+    CalculateResult(numbers, part1Lines, part2Lines);
 
-    var result = CalculateResult(numbers);
+    part1Result += numbers.Last() + part1Lines.Sum();
 
-    long CalculateResult(long[] longs)
+    var part2PartialResult = 0m;
+    for (int i = part2Lines.Count() - 1; i >= 0; i--)
     {
-
+        part2PartialResult = (part2Lines[i] - part2PartialResult);
     }
-
-    var stop = false;
-    while (!stop)
-    {
-        Console.WriteLine($"Doing shit {string.Join(" ", differences)}");
-        differences = GetDifferencesArray(differences);
-        stop = differences.Distinct().Count() == 1;
-    }
-
-
-    Console.WriteLine("DONE");
+    part2Result += numbers.First() - part2PartialResult;
 }
 
-long[] GetDifferencesArray(long[] input)
-{
-    var result = Array.Empty<long>();
-    for (int i = 0; i < input.Length - 1; i++)
-    {
-        if (input[i] == input.Length - 1)
-        {
-            continue;
-        }
+Console.WriteLine($"Part 1 result: {part1Result}");
+Console.WriteLine($"Part 2 result: {part2Result}");
 
-        result = result.Append(input[i + 1] - input[i]).ToArray();
+return;
+
+void CalculateResult(List<long> input, List<long> part1Results, List<long> part2Results)
+{
+    Console.WriteLine(string.Join(" ", input));
+    if (input.Count() == 1)
+    {
+        part1Results.Add(input.Last());
+        part2Results.Add(input.First());
+        return;
     }
 
-    return result;
+    var differences = new List<long>();
+    for (var i = 0; i < input.Count() - 1; i++)
+    {
+        differences.Add(input[i + 1] - input[i]);
+    }
+
+    if (differences.Distinct().Count() == 1)
+    {
+        Console.WriteLine(string.Join(" ", differences));
+        part1Results.Add(differences.Last());
+        part2Results.Add(differences.First());
+        return;
+    }
+
+    part1Results.Add(differences.Last());
+    part2Results.Add(differences.First());
+    CalculateResult(differences, part1Results, part2Results);
 }
