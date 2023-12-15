d44c638e28d7f26f957f5986f029751d884ec669
Emilio Bertò <emilio.berto1@gmail.com>
2023-12-15 01:03:34 +0100

    Another trashy but working code

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -17,10 +17,10 @@ var cardsValue = new Dictionary<char, char>
     { '8', 'G' },
     { '7', 'H' },
     { '6', 'I' },
-    { '5', 'J' },
-    { '4', 'K' },
-    { '3', 'L' },
-    { '2', 'M' },
+    { '5', 'L' },
+    { '4', 'M' },
+    { '3', 'N' },
+    { '2', 'O' },
 };
 
 var summaryList = new List<HandSummary>();
@@ -30,41 +30,7 @@ await foreach (var line in File.ReadLinesAsync(filePath))
     var hand = match.Groups["Hand"].Value;
     var bid = long.Parse(match.Groups["Bid"].Value);
 
-    var groupedCards = hand.ToCharArray().Select(e => e.ToString()).GroupBy(e => e);
-    var handValue = HandValue.HighCard;
-
-    if (groupedCards.Count() == 1)
-    {
-        handValue = HandValue.FiveOfAKind;
-    }
-    else if (groupedCards.Count() == 2)
-    {
-        if (groupedCards.Any(e => e.Count() == 4))
-        {
-            handValue = HandValue.FourOfAKind;
-        }
-
-        if (groupedCards.Any(e => e.Count() == 3))
-        {
-            handValue = HandValue.FullHouse;
-        }
-    }
-    else if (groupedCards.Count() == 3 && groupedCards.Any(e => e.Count() == 3))
-    {
-        handValue = HandValue.ThreeOfAKind;
-    }
-    else if (groupedCards.Count() == 3 && groupedCards.Where(e => e.Count() == 2).Count() == 2)
-    {
-        handValue = HandValue.TwoPair;
-    }
-    else if (groupedCards.Count() == 4)
-    {
-        handValue = HandValue.OnePair;
-    }
-    else if (groupedCards.Count() == 5)
-    {
-        handValue = HandValue.HighCard;
-    }
+    var handValue = CalculateHandValue(hand.ToCharArray().Select(e => e.ToString()).GroupBy(e => e));
 
     var handWithReplacedCards = hand!;
     foreach (var cardKeyValue in cardsValue)
@@ -77,6 +43,7 @@ await foreach (var line in File.ReadLinesAsync(filePath))
         {
             Bid = bid,
             Hand = handWithReplacedCards,
+            OriginalHand = hand,
             HandValue = handValue
         }
     );
@@ -94,20 +61,64 @@ for (int i = 1; i <= orderedSummary.Length; i++)
     part1Result += (orderedSummary[i - 1].Bid * (i));
 }
 
-
 Console.WriteLine($"Part 1 result: {part1Result}");
+
+orderedSummary = summaryList.Select(handSummary =>
+{
+    if (!handSummary.OriginalHand.Contains("J"))
+    {
+        return handSummary;
+    }
+
+    var jokers = handSummary.OriginalHand.Where(e => e == 'J').ToArray();
+    handSummary.HandValue = CalculateHandValue(handSummary.OriginalHand.ToCharArray().Select(e => e.ToString()).Where(e => e is not "J").GroupBy(e => e));
+    cardsValue.TryGetValue('J', out var oldJokerMappedValue);
+    foreach (var joker in jokers)
+    {
+        handSummary.HandValue = handSummary.HandValue switch
+        {
+            HandValue.None => HandValue.HighCard,
+            HandValue.HighCard => HandValue.OnePair,
+            HandValue.OnePair => HandValue.ThreeOfAKind,
+            HandValue.TwoPair => HandValue.FullHouse,
+            HandValue.ThreeOfAKind => HandValue.FourOfAKind,
+            HandValue.FourOfAKind => HandValue.FiveOfAKind,
+            HandValue.FullHouse => HandValue.FullHouse,
+            HandValue.FiveOfAKind => HandValue.FiveOfAKind,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    handSummary.Hand = handSummary.Hand.Replace(oldJokerMappedValue.ToString(), "Z");
+    return handSummary;
+}).ToArray();
+
+
+orderedSummary = orderedSummary
+    .OrderBy(e => e.HandValue)
+    .GroupBy(e => e.HandValue)
+    .Select(e => e.OrderByDescending(handSummary => handSummary.Hand))
+    .SelectMany(e => e.ToArray())
+    .ToArray();
+
+for (int i = 1; i <= orderedSummary.Length; i++)
+{
+    part2Result += (orderedSummary[i - 1].Bid * (i));
+}
 
 Console.WriteLine($"Part 2 result: {part2Result}");
 
 internal record HandSummary
 {
     public string Hand { get; set; } = null!;
+    public string OriginalHand { get; set; } = null!;
     public long Bid { get; set; }
     public HandValue HandValue { get; set; }
 }
 
 internal enum HandValue
 {
+    None = 0,
     HighCard = 1,
     OnePair = 2,
     TwoPair = 3,
@@ -115,4 +126,49 @@ internal enum HandValue
     FullHouse = 5,
     FourOfAKind = 6,
     FiveOfAKind = 7
+}
+
+internal partial class Program
+{
+    static HandValue CalculateHandValue(IEnumerable<IGrouping<string, string>> hand)
+    {
+        hand = hand.ToArray();
+
+        if (!hand.Any())
+        {
+            return HandValue.None;
+        }
+
+        if (hand.Any(e => e.Count() == 5))
+        {
+            return HandValue.FiveOfAKind;
+        }
+
+        if (hand.Any(e => e.Count() == 4))
+        {
+            return HandValue.FourOfAKind;
+        }
+
+        if (hand.Any(e => e.Count() == 3) && hand.Any(e => e.Count() == 2))
+        {
+            return HandValue.FullHouse;
+        }
+
+        if (hand.Any(e => e.Count() == 3) && !hand.Any(e => e.Count() == 2))
+        {
+            return HandValue.ThreeOfAKind;
+        }
+
+        if (hand.Count(e => e.Count() == 2) == 2)
+        {
+            return HandValue.TwoPair;
+        }
+
+        if (hand.Count(e => e.Count() == 2) == 1)
+        {
+            return HandValue.OnePair;
+        }
+
+        return HandValue.HighCard;
+    }
 }
