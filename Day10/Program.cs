1d110805e1dffe73a5da4db94c80548fed537e27
Emilio Bertò <emilio.berto1@gmail.com>
2023-12-19 00:00:51 +0100

    Son of a plumber

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -9,35 +9,79 @@ await foreach (var line in File.ReadLinesAsync(filePath))
     matrix = matrix.Append(line.ToCharArray().Select(e => e.ToString()).ToArray()).ToArray();
 }
 
-(int y, int x) startingPosition = (0, 0);
-for (var y = 0; y < matrix.Length; y++)
+var resetted = -1;
+(int y, int x) start = (0, 0);
+while (resetted != 0)
 {
-    var row = matrix[y];
-    for (var x = 0; x < row.Length; x++)
+    resetted = 0;
+    for (var y = 0; y < matrix.Length; y++)
     {
-        if (row[x] is not StartingPosition)
+        var row = matrix[y];
+        for (var x = 0; x < row.Length; x++)
         {
-            if (GetConnectingCells(matrix, y, x).Count == 0)
+            if (row[x] is not StartingPosition)
             {
-                matrix[y][x] = Ground;
+                continue;
             }
-            continue;
-        }
 
-        startingPosition = (y, x);
+            start = (y, x);
+        }
     }
 }
 
-var result = FindPath(matrix, startingPosition.y, startingPosition.x, new List<(int y, int x, string part)>()
+var currentPosition = start;
+List<PathNode> foundPath = new() { new PathNode(start.y, start.x, matrix[start.y][start.x]) };
+while (foundPath.Count <= 3 || foundPath.Last().Part is not StartingPosition)
 {
-    (startingPosition.y, startingPosition.x, StartingPosition)
-});
+    var connectingCells = GetConnectingCells(matrix, currentPosition.y, currentPosition.x)
+        .Where(node => !foundPath.Contains(node) || node.Part is StartingPosition && foundPath.Count > 3)
+        .ToList();
 
-PrintMatrixFromPath(matrix, result);
+    if (connectingCells.Count is 0)
+    {
+        foreach (var path in foundPath.Where((node, index) => index > 0))
+        {
+            matrix[path.Y][path.X] = Ground;
+        }
 
-part1Result = (result.Count - 1) / 2;
+        foundPath = new();
+        currentPosition = start;
+        continue;
+    }
+
+    var nextCell = connectingCells.First();
+    foundPath.Add(nextCell);
+    currentPosition = new(nextCell.Y, nextCell.X);
+}
+
+PrintMatrixFromPath(matrix, foundPath);
+part1Result = (foundPath.Count - 1) / 2;
+
+
+// var result = FindPath(matrix, startingPosition.y, startingPosition.x, new List<PathNode>()
+// {
+//     new PathNode(startingPosition.y, startingPosition.x, StartingPosition)
+// });
+//
+// PrintMatrixFromPath(matrix, result);
+//
+// part1Result = (result.Count - 1) / 2;
 
 Console.WriteLine($"Part 1 result: {part1Result}");
+
+internal struct PathNode
+{
+    public int Y { get; set; }
+    public int X { get; set; }
+    public string Part { get; set; }
+
+    public PathNode(int y, int x, string part)
+    {
+        Y = y;
+        X = x;
+        Part = part;
+    }
+}
 
 internal partial class Program
 {
@@ -50,25 +94,27 @@ internal partial class Program
     public const string StartingPosition = "S";
     public const string Ground = ".";
 
-    static List<(int y, int x, string part)> FindPath(string[][] matrix, int y, int x, List<(int y, int x, string part)> path)
+    static List<PathNode> FindPath(string[][] matrix, int y, int x, List<PathNode> path)
     {
-        if (path.Count > 3 && path.First().part is StartingPosition && path.Last().part is StartingPosition)
+        if (path.Count > 3 && path.First().Part is StartingPosition && path.Last().Part is StartingPosition)
         {
             return path;
         }
 
         var connectingCells = GetConnectingCells(matrix, y, x)
-            .Where(e => e.part is StartingPosition && path.Count > 2 || !path.Contains(e)); // Starting position only if closing loop and avoid already used cells.
+            .Where(e => e.Part is StartingPosition && path.Count > 2 || !path.Contains(e)); // Starting position only if closing loop and avoid already used cells.
 
-        var bestPath = new List<(int y, int x, string part)>();
+        var bestPath = new List<PathNode>();
+        List<List<PathNode>> invalidPaths = new();
         foreach (var cell in connectingCells)
         {
             var pathCopy = path.Select(tuple => tuple).Append(cell).ToList();
-            var foundPath = FindPath(matrix, cell.y, cell.x, pathCopy);
+            var foundPath = FindPath(matrix, cell.Y, cell.X, pathCopy);
 
-            // Found a better path...
-            if (foundPath.Count <= path.Count)
+            if (foundPath.Count == path.Count)
             {
+                invalidPaths.Add(foundPath);
+                Console.WriteLine("Invalid path found!");
                 continue;
             }
 
@@ -79,14 +125,14 @@ internal partial class Program
         return bestPath;
     }
 
-    private static void PrintMatrixFromPath(string[][] matrix, List<(int y, int x, string part)> path)
+    private static void PrintMatrixFromPath(string[][] matrix, List<PathNode> path)
     {
         var matrixCopy = matrix.Select(e => e.Select(x => x).ToArray()).ToArray();
         for (int y = 0; y < matrixCopy.Length; y++)
         {
             for (int x = 0; x < matrixCopy[y].Length; x++)
             {
-                var isInPath = path.Contains((y, x, matrixCopy[y][x]));
+                var isInPath = path.Contains(new(y, x, matrixCopy[y][x]));
                 if (!isInPath)
                 {
                     matrixCopy[y][x] = Ground;
@@ -100,60 +146,7 @@ internal partial class Program
         }
     }
 
-    private List<(int y, int x, string part)> FindClosingPath(string[][] matrix, int y, int x, List<(int y, int x, string part)> path)
-    {
-        if (path.Count > 2 && path.First().part is StartingPosition && path.Last().part is StartingPosition)
-        {
-            return path;
-        }
-
-        var connectingPieces = GetConnectingCells(matrix, y, x);
-
-        if (connectingPieces.Count == 0)
-        {
-            return path;
-        }
-
-        var paths = new List<List<(int y, int x, string part)>>();
-        foreach (var connectingPiece in connectingPieces.Where(e => e.part is StartingPosition && path.Count > 2 || !path.Contains(e)))
-        {
-            // Avoid closing circle
-            if (path.Count >= 2 && connectingPiece.part == StartingPosition && path.ElementAt(path.Count - 2).part is StartingPosition)
-            {
-                paths.Add(path);
-                continue;
-            }
-
-            var pathCopy = path.Select(tuple => tuple).Append(connectingPiece).ToList();
-            var foundPath = FindClosingPath(matrix, connectingPiece.y, connectingPiece.x, pathCopy);
-            if (foundPath.Count == 0)
-            {
-                continue;
-            }
-
-            if (foundPath.Count > 2 && foundPath.First().part is StartingPosition && foundPath.Last().part is StartingPosition)
-            {
-                Console.WriteLine(string.Join(" -> ", foundPath));
-                return foundPath;
-            }
-
-            if (foundPath.Count > path.Count)
-            {
-                paths.Add(foundPath);
-                continue;
-            }
-
-
-            paths.Add(path);
-        }
-
-        var longestPath = paths.Count == 0
-            ? new List<(int y, int x, string part)>()
-            : paths.OrderBy(e => e.Count).First();
-        return longestPath;
-    }
-
-    private static List<(int y, int x, string part)> GetConnectingCells(string[][] matrix, int y, int x)
+    private static List<PathNode> GetConnectingCells(string[][] matrix, int y, int x)
     {
         var currentCell = matrix[y][x];
         var topCell = y == 0 ? Ground : matrix[y - 1][x];
@@ -161,26 +154,26 @@ internal partial class Program
         var leftCell = x == 0 ? Ground : matrix[y][x - 1];
         var rightCell = x == matrix[y].Length - 1 ? Ground : matrix[y][x + 1];
 
-        var connectingPieces = new List<(int y, int x, string part)>();
+        var connectingPieces = new List<PathNode>();
 
         if (topCell is not Ground && AreCellsConnected(currentCell, topCell, Direction.South) && AreCellsConnected(topCell, currentCell, Direction.North))
         {
-            connectingPieces.Add((y - 1, x, topCell));
+            connectingPieces.Add(new(y - 1, x, topCell));
         }
 
         if (bottomCell is not Ground && AreCellsConnected(currentCell, bottomCell, Direction.North) && AreCellsConnected(bottomCell, currentCell, Direction.South))
         {
-            connectingPieces.Add((y + 1, x, bottomCell));
+            connectingPieces.Add(new(y + 1, x, bottomCell));
         }
 
         if (leftCell is not Ground && AreCellsConnected(currentCell, leftCell, Direction.East) && AreCellsConnected(leftCell, currentCell, Direction.West))
         {
-            connectingPieces.Add((y, x - 1, leftCell));
+            connectingPieces.Add(new(y, x - 1, leftCell));
         }
 
         if (rightCell is not Ground && AreCellsConnected(currentCell, rightCell, Direction.West) && AreCellsConnected(rightCell, currentCell, Direction.East))
         {
-            connectingPieces.Add((y, x + 1, rightCell));
+            connectingPieces.Add(new(y, x + 1, rightCell));
         }
 
         return connectingPieces;
@@ -211,53 +204,4 @@ internal partial class Program
         var hasValue = Map.TryGetValue(next, out var allowedDirection);
         return hasValue && allowedDirection!.Contains(fromDirection);
     }
-
-    // private static bool CanConnectTo(string current, string next, Direction direction)
-    // {
-    //     if (next is Ground)
-    //     {
-    //         return false;
-    //     }
-    //
-    //     return current switch
-    //     {
-    //         StartingPosition when direction is Direction.North => next is Vertical or TopLeftCorner or TopRightCorner,
-    //         StartingPosition when direction is Direction.South => next is Vertical or BottomLeftCorner or BottomRightCorner,
-    //         StartingPosition when direction is Direction.West => next is Horizontal or BottomLeftCorner or TopLeftCorner,
-    //         StartingPosition when direction is Direction.East => next is Horizontal or BottomRightCorner or TopRightCorner,
-    //         StartingPosition => next is not Ground,
-    //         Ground => false,
-    //
-    //         Vertical when direction is Direction.North => next is Vertical or TopLeftCorner or TopRightCorner or StartingPosition,
-    //         Vertical when direction is Direction.South => next is Vertical or BottomLeftCorner or BottomRightCorner or StartingPosition,
-    //         Vertical when direction is Direction.West => false,
-    //         Vertical when direction is Direction.East => false,
-    //
-    //         Horizontal when direction is Direction.North => false,
-    //         Horizontal when direction is Direction.South => false,
-    //         Horizontal when direction is Direction.West => next is Horizontal or BottomLeftCorner or TopLeftCorner or StartingPosition,
-    //         Horizontal when direction is Direction.East => next is Horizontal or TopRightCorner or BottomRightCorner or StartingPosition,
-    //
-    //         TopLeftCorner when direction is Direction.North => false,
-    //         TopLeftCorner when direction is Direction.West => false,
-    //         TopLeftCorner when direction is Direction.South => next is Vertical or BottomLeftCorner or BottomRightCorner or StartingPosition,
-    //         TopLeftCorner when direction is Direction.East => next is Horizontal or BottomRightCorner or TopRightCorner or StartingPosition,
-    //
-    //         TopRightCorner when direction is Direction.North => false,
-    //         TopRightCorner when direction is Direction.East => false,
-    //         TopRightCorner when direction is Direction.West => next is Horizontal or BottomLeftCorner or TopLeftCorner or StartingPosition,
-    //         TopRightCorner when direction is Direction.South => next is Vertical or BottomLeftCorner or BottomRightCorner or StartingPosition,
-    //
-    //         BottomLeftCorner when direction is Direction.West => false,
-    //         BottomLeftCorner when direction is Direction.South => false,
-    //         BottomLeftCorner when direction is Direction.North => next is Vertical or TopLeftCorner or TopRightCorner or StartingPosition,
-    //         BottomLeftCorner when direction is Direction.East => next is Horizontal or TopRightCorner or BottomRightCorner or StartingPosition,
-    //
-    //         BottomRightCorner when direction is Direction.East => false,
-    //         BottomRightCorner when direction is Direction.South => false,
-    //         BottomRightCorner when direction is Direction.North => next is Vertical or TopLeftCorner or TopRightCorner or StartingPosition,
-    //         BottomRightCorner when direction is Direction.West => next is Horizontal or TopLeftCorner or BottomLeftCorner or StartingPosition,
-    //         _ => throw new ArgumentOutOfRangeException(nameof(current), current, null)
-    //     };
-    // }
 }
